8a1f099c7d62372634d5077f6834594fe2ef7eb2
AurelBichop <linux.aurelien@gmail.com>
2020-09-03 10:52:55 +0200

    fin du Range

--- main.go
+++ main.go
@@ -5,33 +5,12 @@ import (
 )
 
 func main() {
-	sum := 0
-	for i := 1; i <= 10; i++ {
-		sum += i
-	}
-	fmt.Printf("Sum result= %v\n", sum)
-
-	eventCnt := 0
-	for eventCnt < 3 {
-		fmt.Println("Retrieving events")
-		eventCnt++
-		if eventCnt == 3 {
-			fmt.Printf("Got %d notifications \n", eventCnt)
-		}
+	names := []string{"Aby", "Raph", "Adrien", "Aurel"}
+	for i, n := range names {
+		fmt.Printf("Username=%s (index=%d)\n", n, i)
 	}
 
-	i := 0
-	for {
-		i++
-		if i%2 != 0 {
-			fmt.Println("odd looping")
-			continue
-		}
-		fmt.Println("Loopinq ...")
-
-		if i >= 10 {
-			fmt.Println("Loop END")
-			break
-		}
+	for _, c := range "golang" {
+		fmt.Printf("%v\n", string(c))
 	}
 }
