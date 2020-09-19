b312a720b293978dcda2b079ab4f4b8238d7f53d
Aurelien <linux.aurelien@gmail.com>
2020-09-19 10:14:30 +0200

    definition d'une map

--- main.go
+++ main.go
@@ -1,54 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-type Post struct {
-	Title     string
-	Text      string
-	published bool
-}
-
-func (p Post) Headline() string {
-	return fmt.Sprintf("%v - %v", p.Title, p.Text[:50])
-}
-
-func (p *Post) Published() bool {
-	return p.published
-}
-
-func (p *Post) Publish() {
-	p.published = true
-}
-
-func (p *Post) UnPublished() {
-	p.published = false
-}
-
-func UpperTitle(p *Post) {
-	p.Title = strings.ToUpper(p.Title)
+type Vector struct {
+	X, Y int
 }
 
 func main() {
-	p := Post{
-		Title: "Go releaser",
-		Text: `Lorem ipsum edsz fzefze f ze
-efze fez fzef zef ze f f zez  ezf ezfzef ezf zfpezkfopzejoàfuziçfh uguzgfygzeyuqafgqeyfgqzgg qfg uiq`,
-	}
+	//var m map[int]bool = make(map[int]bool)
+	//var m2 = make(map[int]bool)
 
-	fmt.Println(p.Headline())
-	fmt.Printf("Post Published %v\n", p.Published())
+	m3 := make(map[int]bool)
+	fmt.Println(m3)
 
-	p.Publish()
-	fmt.Printf("Post Published %v\n", p.Published())
-
-	pythonPost := &Post{
-		Title: "Python releaser",
-		Text: `Lorem ipsum edsz fzefze f ze
-efze fez fzef zef fuhdhsfgyfd ezf ezfzef ezf zfpezkfopzejoàfuziçfh uguzgfygzeyuqafgqeyfgqzgg qfg uiq`,
-	}
-	UpperTitle(pythonPost)
-	fmt.Println(pythonPost.Headline())
+	vecs := make(map[Vector]string)
+	fmt.Println(vecs)
 }
