feefeb71635f711809d558d9269bb0bbe3707dfa
Aurelien <linux.aurelien@gmail.com>
2020-09-19 10:58:14 +0200

    modifier une map

--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import "fmt"
 
-type Vector struct {
-	X, Y int
-}
-
 func main() {
-	//var m map[int]bool = make(map[int]bool)
-	//var m2 = make(map[int]bool)
+	m := make(map[string]int)
+	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
 
-	m3 := make(map[int]bool)
-	fmt.Println(m3)
+	m["hello"] = 5
+	m["GoodBye"] = 10
+	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
 
-	vecs := make(map[Vector]string)
-	fmt.Println(vecs)
+	fmt.Printf("key hello, value=%v\n", m["hello"])
+	i := m["GoodBye"]
+	fmt.Println(i)
+
+	j, ok := m["yelo"]
+	fmt.Printf("j=%v, ok=%v\n", j, ok)
+
+	//Pour tester l'égalité
+	m["beatles"] = 2
+	if _, ok := m["beatles"]; ok {
+		fmt.Println("Beatles key exist")
+	}
+
+	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
+
+	delete(m, "beatles")
+	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
+
+	m2 := m
+	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
+
+	m2["help"] = 44
+
+	fmt.Printf("m content %v (len=%v)\n", m, len(m))
+	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
 }
