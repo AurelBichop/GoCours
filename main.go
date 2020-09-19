c47912a775a66d592a980efc95bed5c1f099eed1
Aurelien <linux.aurelien@gmail.com>
2020-09-19 11:06:58 +0200

    parcourir une map

--- main.go
+++ main.go
@@ -3,36 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	m := make(map[string]int)
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
-
-	m["hello"] = 5
-	m["GoodBye"] = 10
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
-
-	fmt.Printf("key hello, value=%v\n", m["hello"])
-	i := m["GoodBye"]
-	fmt.Println(i)
-
-	j, ok := m["yelo"]
-	fmt.Printf("j=%v, ok=%v\n", j, ok)
-
-	//Pour tester l'égalité
-	m["beatles"] = 2
-	if _, ok := m["beatles"]; ok {
-		fmt.Println("Beatles key exist")
+	m := map[string]int{
+		"Adrien": 7,
+		"Raph":   33,
+		"Aurel":  27,
+		"Aby":    11,
 	}
 
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
+	for name, age := range m {
+		fmt.Printf("name=%s, age=%d\n", name, age)
+	}
 
-	delete(m, "beatles")
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
+	i := 1
+	for name := range m {
+		fmt.Printf("name=%s\n", name)
+		m[name] = i
+		i++
+	}
 
-	m2 := m
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
-
-	m2["help"] = 44
-
-	fmt.Printf("m content %v (len=%v)\n", m, len(m))
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
+	fmt.Println(m)
 }
